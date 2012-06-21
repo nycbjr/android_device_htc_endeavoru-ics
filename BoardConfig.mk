ee87ea5f13cdc15fe87568a4d90d62fdf3680bb4
nycbjr <nycbjr@gmail.com>
2012-06-21 07:14:47 -0400

    initial push of aokp modd'd device tree for onex

---
+++ BoardConfig.mk
@@ -0,0 +1,97 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Skip droiddoc build to save build time
+BOARD_SKIP_ANDROID_DOC_BUILD := true
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+#TARGET_PROVIDES_LIBAUDIO := true
+#TARGET_PROVIDES_LIBLIGHTS := true
+BOARD_USES_ALSA_AUDIO := true
+BUILD_WITH_ALSA_UTILS := true
+
+#Camera
+BOARD_HAVE_HTC_FFC := true
+USE_CAMERA_STUB := false
+
+# Target arch settings
+BOARD_HAS_LOCKED_BOOTLOADER := true
+TARGET_NO_BOOTLOADER := true
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+# Board nameing
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOOTLOADER_BOARD_NAME := 
+TARGET_BOARD_PLATFORM := tegra
+TARGET_TEGRA_VERSION := t30
+
+# mediaplayer
+BOARD_USES_HW_MEDIAPLUGINS := false
+BOARD_USES_HW_MEDIASCANNER := false
+BOARD_USES_HW_MEDIARECORDER := false
+
+# EGL settings
+TARGET_USES_OVERLAY := true
+USE_OPENGL_RENDERER := true
+BOARD_USE_SKIA_LCDTEXT := true
+BOARD_NO_ALLOW_DEQUEUE_CURRENT_BUFFER := true
+BOARD_EGL_CFG := device/htc/endeavoru/configs/egl.cfg
+
+# Wi-Fi
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WLAN_DEVICE := wl12xx_mac80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_wl12xx
+BOARD_SOFTAP_DEVICE_TI := NL80211
+WIFI_DRIVER_MODULE_NAME	:=  "wl12xx_sdio"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wl12xx_sdio.ko"
+
+# RIL
+BOARD_PROVIDES_LIBRIL := true
+
+# BT
+BOARD_HAVE_BLUETOOTH := true
+
+# FM
+# BOARD_HAVE_FM_RADIO_TI := true
+
+BOARD_HDMI_MIRROR_MODE := Scale
+BOARD_HAS_DIDIM := true
+
+# Vold / USB
+BOARD_VOLD_MAX_PARTITIONS := 20
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/fsl-tegra-udc/gadget/lun0/file
+
+# Kernel / Ramdisk
+TARGET_PREBUILT_KERNEL := device/htc/endeavoru/prebuilt/kernel
+TARGET_PROVIDES_INIT_TARGET_RC := true
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1342177280
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2302672896
+BOARD_FLASH_BLOCK_SIZE := 4096
