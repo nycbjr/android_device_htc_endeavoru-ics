0c31d49319976b55e9ac2d6e608da0d960525e3a
nycbjr <nycbjr@gmail.com>
2012-06-27 22:08:59 -0400

    misc clean up and vol fixes

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -12,20 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 LOCAL_PATH:= $(call my-dir)
-# HAL module implemenation stored in
-# hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
+
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := lights.c
-
+LOCAL_MODULE := lights.tegra
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
-LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE_TAGS := optional
 
-LOCAL_MODULE := lights.endeavoru
+LOCAL_SRC_FILES := lights.c
+LOCAL_SHARED_LIBRARIES := liblog
 
 LOCAL_MODULE_TAGS := optional
 
